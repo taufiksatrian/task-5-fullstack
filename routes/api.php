c7848bcbb4ef15e9f9391ac3df0468d2d2a94d53
taufiksatrian <taufiksatrianugraha21@gmail.com>
2022-09-26 16:25:16 +0700

    komit

---
+++ routes/api.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
+
+Route::controller(TodoController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todo', 'store');
+    Route::get('todo/{id}', 'show');
+    Route::put('todo/{id}', 'update');
+    Route::delete('todo/{id}', 'destroy');
+});
+
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+
+/**
+ * route "/register"
+ * @method "POST"
+ */
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ * route "/logout"
+ * @method "POST"
+ */
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
